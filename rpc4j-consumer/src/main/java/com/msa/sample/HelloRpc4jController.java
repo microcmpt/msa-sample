7f23b39ff354105592b7691b5943253269516aa7
xiaping.shen <xiaping.shen@anxincloud.com>
2018-05-18 15:02:41 +0800

    添加rpc4j使用例子

--- rpc4j-consumer/src/main/java/com/msa/sample/HelloRpc4jController.java
+++ rpc4j-consumer/src/main/java/com/msa/sample/HelloRpc4jController.java
@@ -2,9 +2,7 @@ package com.msa.sample;
 
 import com.msa.sample.api.HelloRpc4jService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * The type Hello rpc 4 j controller.
@@ -15,21 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version: 1.0.0
  */
 @RestController
+@RequestMapping("/api")
 public class HelloRpc4jController {
-//    /**
-//     * The Hello rpc 4 j service.
-//     */
-//    @Autowired
-//	private HelloRpc4jService helloRpc4jService;
-//
-//    /**
-//     * Hello string.
-//     *
-//     * @param name the name
-//     * @return the string
-//     */
-//    @GetMapping(value = "/api/hello/{name}")
-//    public String hello(@PathVariable("name")String name) {
-//        return helloRpc4jService.hello(name);
-//    }
+    /**
+     * The Hello rpc 4 j service.
+     */
+    @Autowired
+	private HelloRpc4jService helloRpc4jService;
+
+    /**
+     * Hello string.
+     *
+     * @param name the name
+     * @return the string
+     */
+    @RequestMapping(method = RequestMethod.GET, value = "/hello/{name}")
+    public String hello(@PathVariable("name")String name) {
+        return helloRpc4jService.hello(name);
+    }
 }
--- rpc4j-consumer/src/main/java/com/msa/sample/Rpc4jClientSampleApplication.java
+++ rpc4j-consumer/src/main/java/com/msa/sample/Rpc4jClientSampleApplication.java
@@ -1,26 +1,17 @@
 package com.msa.sample;
 
 import com.msa.rpc4j.annotation.EnableRpc4jClients;
-import com.msa.sample.api.HelloRpc4jService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.PostConstruct;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * The type Msa sample application.
  */
 @SpringBootApplication
-@EnableRpc4jClients(basePackages = "com.msa.sample")
+@ComponentScan(basePackages = "com.msa.sample")
+@EnableRpc4jClients(basePackages = "com.msa.sample.api")
 public class Rpc4jClientSampleApplication {
-	@Autowired
-	private HelloRpc4jService helloRpc4jService;
-
-	@PostConstruct
-	public void init() {
-		System.out.println(helloRpc4jService.hello("sxp"));
-	}
 	/**
 	 * The entry point of application.
 	 *
