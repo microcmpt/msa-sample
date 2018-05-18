80e1cc72e473cb26a0bf241993baf267c5ebd2f7
xiaping.shen <xiaping.shen@anxincloud.com>
2018-05-18 15:56:54 +0800

    添加rpc4j使用例子

--- rpc4j-consumer/src/main/java/com/msa/sample/HelloRpc4jController.java
+++ rpc4j-consumer/src/main/java/com/msa/sample/HelloRpc4jController.java
@@ -1,8 +1,10 @@
 package com.msa.sample;
 
-import com.msa.sample.api.HelloRpc4jService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * The type Hello rpc 4 j controller.
